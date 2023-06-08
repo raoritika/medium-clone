cb92a9a21ff40c426313d4f8541d0efde9dc3818
Ritika Rao <ritikarao0903@gmail.com>
2023-06-08 11:38:10 +0530

    Add foldername to repository

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const url = "mongodb://127.0.0.1/medium";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
